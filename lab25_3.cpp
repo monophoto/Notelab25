0a24d206650ad3670a53c530d609c62e930c0454
Jetsadawut Papherata <jetsadawut1013@gmail.com>
2023-03-30 06:41:05 -0700

    lab25complete

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[],int size) {
+	int cow = 0;
+	set<int> DATA;
+	for(int i = 0;i < size;i++) {
+		DATA.insert(data[i]);
+	}
+
+	set<int>::iterator i;
+	for(i = DATA.begin();i != DATA.end();i++) {
+		cow++;
+	}
+	return cow;
+
+
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
